15feb79b9288ce9be8ac94902ac50d296f74ecdc
Fehér Gergő Márk 627 <fehger627@hengersor.hu>
2025-03-19 11:56:10 +0100

    alap

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const { request } = require("http");
+
+app.use(cors());
+app.use(bodyparser.json());
+
+
+
+const db = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        port: "3306",
+        password: "",
+        database:"",
+    }
+
+)
+app.get("/", (req, res) => {
+    res.send("Fut a server")
+}
+
+)
+
+
+
+app.listen(7172, () => {
+    console.log('A szerver a 8080 porton fut!')
+});
